8700686c68390d62138ea2591a263d2d51ede643
Ffinnis <potapovroman03@gmail.com>
2026-09-19 23:00:17 +0700

    Initial Commit

---
+++ opendoc/opendocApp.swift
@@ -0,0 +1,32 @@
+//
+//  opendocApp.swift
+//  opendoc
+//
+//  Created by Roman on 19.09.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct opendocApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
